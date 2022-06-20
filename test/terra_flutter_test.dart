724b065138059d8ee323ac5e0d9f82ce2bf71fa7
Elliott Yu <elliottyu1@gmail.com>
2022-06-20 16:07:02 +0100

    Flutter push

--- test/terra_flutter_test.dart
+++ test/terra_flutter_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:terra_flutter/terra_flutter.dart';
+import 'package:terra_flutter_bridge/terra_flutter.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('terra_flutter');
