60f00b47ff6abe2e30c8d8798ccbe247f919e6dd
Elliott Yu <57838618+elliottyu19@users.noreply.github.com>
2026-06-12 18:16:34 +0100

    Fix Android initConnection to return native success state

--- android/src/main/java/co/tryterra/terra_flutter_bridge/TerraFlutterPlugin.java
+++ android/src/main/java/co/tryterra/terra_flutter_bridge/TerraFlutterPlugin.java
@@ -211,7 +211,7 @@ public class TerraFlutterPlugin implements FlutterPlugin, MethodCallHandler, Act
       null,
       (success, error)-> {
         HashMap<String, Object> map = new HashMap<>();
-        map.put("success", true);
+        map.put("success", success);
         if (error != null){
           map.put("error", error.getMessage());
         }
